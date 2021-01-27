abf58b7d5659412b26d10593d055a8c35f4d2e14
Agostina Caranta <agostinacaranta@gmail.com>
2021-01-27 17:09:42 -0300

    Commit ejercicio Git clase 11 uso de cssSelector and placeholder

---
+++ ejGitClase11.java
@@ -0,0 +1,40 @@
+package clase11;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ejGitClase11 {
+
+    private WebDriver getDriver(String URL) {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+        return driver;
+    }
+    //ejercicio 1
+    @Test
+    public void spotifyByPlaceHolder(){
+
+        WebDriver driver = getDriver("https://www.spotify.com/uy/signup/");
+        driver.findElement(By.cssSelector("input[placeholder='Introduce tu correo electrónico.']")).sendKeys("test@test.com");
+        driver.findElement(By.cssSelector("input[placeholder='Vuelve a introducir tu correo electrónico.']")).sendKeys("test@test.com");
+        driver.findElement(By.cssSelector("input[placeholder='Crea una contraseña.']")).sendKeys("contrasena");
+        driver.findElement(By.cssSelector("input[placeholder='Introduce un nombre de perfil.']")).sendKeys("marimar");
+        driver.findElement(By.cssSelector("input[placeholder='DD']")).sendKeys("24");
+
+        WebElement mes = driver.findElement(By.xpath("//*[@name='month']"));
+        Select mesDropDown = new Select(mes);
+        mesDropDown.selectByValue("02");
+
+        driver.findElement(By.cssSelector("input[placeholder='AAAA']")).sendKeys("1984");
+
+        WebElement radio = driver.findElement(By.xpath("(//*[@class='Indicator-sc-16vj7o8-0 kSKYRE'])[2]"));
+        radio.click();
+
+        driver.close();
+    }
+}
